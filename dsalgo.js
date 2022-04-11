072c0ad2965563c65770e7143f31f7a3d818c38b
Ben Jackson <benjacksonSE@gmail.com>
2022-04-11 09:05:14 -0400

    sumZero

--- dsalgo.js
+++ dsalgo.js
@@ -87,3 +87,18 @@ function validAnagram(first, second) {
 
 
 //O(n) time
+
+//Given an array of storted integers return the first pair that adds up to zero
+
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+
+// sumZero([-4,-3,-2,-1,0,1,2,5])
